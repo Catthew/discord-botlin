6998862b0a3e5be339b859f88c07cc4b1c6ae3ac
Matthew Wolfman <illya@matthews-mini.myfiosgateway.com>
2021-06-06 14:06:12 -0400

    added more messages and reset stats time

--- cron_jobs/index.js
+++ cron_jobs/index.js
@@ -10,18 +10,22 @@ module.exports = client => {
 
     //Thursdays at 6:00 PM
     cron.schedule('0 18 * * 4', () => {
+        console.log('Sending 1st schedule alert');
         client.channels.cache.get(channel).send(`${prefix} schedule`).catch(console.error);
+        console.log('Complete');
     });
     //Saturdays at 12:00 PM
     cron.schedule('0 12 * * 6', () => {
+        console.log('Sending 2nd schedule alert');
         client.channels.cache.get(channel).send(`${prefix} schedule`).catch(console.error);
+        console.log('Complete');
     });
     //Sunday at 00:00 AM
-    cron.schedule('03 14 * * 7', () => {
+    cron.schedule('0 0 * * 7', () => {
         console.log('Updating the stats');
         let spreadsheet = process.env.SPREADSHEET;
         var e_stats = require('./excel_stats')(spreadsheet, client);
-        console.log('Done');
+        console.log('Complete');
     });
 
     app.listen(port);
