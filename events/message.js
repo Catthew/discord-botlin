b905f20fb29de0ab9be3d291a42555769f82a484
Matthew Wolfman <illya@matthews-mini.myfiosgateway.com>
2021-06-06 14:13:56 -0400

    made prefix a variable

--- events/message.js
+++ events/message.js
@@ -1,16 +1,17 @@
 module.exports = (client, message) => {
+    const prefix = process.env.PREFIX;
     if (message.author.bot) {
         let current_message = new String(message.content).trim();
-        const allowed_commands = (`${process.env.PREFIX} schedule`, `${process.env.PREFIX} stats`);
+        const allowed_commands = (`${prefix} schedule`, `${prefix} stats`);
         if(!allowed_commands.includes(current_message)){
             return;
         }
     }
 
     if (message.content.includes(client.user.id)) message.channel.send(process.env.AT);
-    if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+    if (message.content.indexOf(prefix) !== 0) return;
 
-    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     const cmd = client.commands.get(command);
