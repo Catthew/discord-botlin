839d8a0e4b0ac983f364ed145af04a9ffd4be8b9
Catthew <matthew.wolfman@outlook.com>
2021-07-03 00:27:04 -0400

    Added Drink Schedule Cron

--- cron_jobs/index.js
+++ cron_jobs/index.js
@@ -8,19 +8,37 @@ module.exports = client => {
     let channel = process.env.CHANNEL;
     let port = process.env.PORT;
 
-    //Thursdays at 6:00 PM (+4 hours due to server)
+    /*
+        Send the first schedule alert
+        Thursday at 6:00 PM (+4 hours due to server)
+    */
     cron.schedule('0 22 * * 4', () => {
         console.log('Sending 1st schedule alert');
         client.channels.cache.get(channel).send(`${prefix} schedule`).catch(console.error);
         console.log('Complete');
     });
-    //Saturdays at 12:00 PM (+4 hours due to server)
+    /*
+        Send the second schedule alert
+        Saturday at 12:00 PM (+4 hours due to server)
+    */
     cron.schedule('0 16 * * 6', () => {
         console.log('Sending 2nd schedule alert');
         client.channels.cache.get(channel).send(`${prefix} schedule`).catch(console.error);
         console.log('Complete');
     });
-    //Sunday at 00:00 AM (+4 hours due to server)
+    /*
+        Update the drink schedule
+        Saturday at 11:00 PM (+4 hours due to server)
+    */
+    cron.schedule('0 3 * * 7', () => {
+        console.log('Updating the drink schedule');
+        require('./schedule_update')(client);
+        console.log('Complete');
+    });
+    /*
+        Send the stats
+        Sunday at 00:00 AM (+4 hours due to server)
+    */
     cron.schedule('0 4 * * 7', () => {
         console.log('Updating the stats');
         require('./excel_stats')(process.env.SPREADSHEET, client);
