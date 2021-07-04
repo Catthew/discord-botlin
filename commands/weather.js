1eb69cf29b4d258b7284541eaac864a7b23cd080
Catthew <matthew.wolfman@outlook.com>
2021-07-04 00:19:35 -0400

    Change some let to const

--- commands/weather.js
+++ commands/weather.js
@@ -14,14 +14,14 @@ exports.help = {
  * @param {Discord.Message} message The message object that triggered this method.
  */
 exports.run = async (args, client, message) => {
-    let currentWeather = await fetch(`http://api.weatherstack.com/current?access_key=${process.env.WEATHER}&query=New York`)
+    const currentWeather = await fetch(`http://api.weatherstack.com/current?access_key=${process.env.WEATHER}&query=New York`)
         .then(res => res.json());
     const degree = String.fromCharCode(176);
     const location = currentWeather.location.name;
     const temp = parseInt(currentWeather.current.temperature) * 9 / 5 + 32;
     const weatherDesc = currentWeather.current.weather_descriptions[0];
     const weatherIcon = currentWeather.current.weather_icons[0];
-    let embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setColor('#7289da')
         .setDescription(`${temp}${degree}F and ${weatherDesc}`)
         .setThumbnail(weatherIcon)
