bc1f5a8a196dc14f47e03c89504c05292e927879
Catthew <matthew.wolfman@outlook.com>
2021-09-26 18:22:50 -0400

    Added a file for bot responses

---
+++ responses/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    access_denied: 'You do not have permission to make that command.',
+    bot_no_command: 'Do not talk to me.',
+    disconnected: 'Botlin has disconnected.',
+    no_command: 'Please enter a command.',
+    unknown_command: 'I do not know this command.'
+};
