f0177a669d9ac4754a23f963a2d2e7d120bf2318
Catthew <matthew.wolfman@outlook.com>
2022-04-25 22:58:26 -0400

    Fixed the stats

--- test/commands/schedule/time/server_offset.test.js
+++ test/commands/schedule/time/server_offset.test.js
@@ -2,9 +2,9 @@ const expect = require('chai').expect;
 const tests = require('../../../../commands/schedule/schedule_time');
 
 describe('schedule_time.serverOffset() Test', () => {
-    it('Should return 9', () => {
+    it('Should return 8', () => {
         const hour = 4;
         const result = tests.serverOffset(hour);
-        expect(result).to.equal(9);
+        expect(result).to.equal(8);
     });
 });
--- test/commands/stats/array_to_string.test.js
+++ test/commands/stats/array_to_string.test.js
@@ -7,7 +7,18 @@ describe('stats.topArrayToString() Test', () => {
             name: 'Test',
             value: '80'
         }];
-        const result = tests.arrayToString(testArray, 'value', 100);
+        const result = tests.arrayToString(testArray, 'value', 100, false);
         expect(result).to.equal('Test: 80 (80%)\n');
+    });
+
+    it('should equal Test: 3 (3%)\n', () => {
+        const testArray = [{
+            name: 'Test',
+            optionalStats: {
+                redCoins: 3
+            }
+        }];
+        const result = tests.arrayToString(testArray, 'redCoins', 100, true);
+        expect(result).to.equal('Test: 3 (3%)\n');
     });
 });
