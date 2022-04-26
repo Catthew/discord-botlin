3515523f291fb4d4a3a81586d84abce979d3531f
Catthew <matthew.wolfman@outlook.com>
2022-04-25 22:59:46 -0400

    Updated Test

--- test/commands/stats/array_to_string.test.js
+++ test/commands/stats/array_to_string.test.js
@@ -2,23 +2,23 @@ const expect = require('chai').expect;
 const tests = require('../../../commands/stats/stats_info');
 
 describe('stats.topArrayToString() Test', () => {
-    it('should equal Test: 80 (80%)\n', () => {
+    it('should equal damageTaken: 80 (80%)\n', () => {
         const testArray = [{
-            name: 'Test',
-            value: '80'
+            name: 'damageTaken',
+            damageTaken: '80'
         }];
-        const result = tests.arrayToString(testArray, 'value', 100, false);
-        expect(result).to.equal('Test: 80 (80%)\n');
+        const result = tests.arrayToString(testArray, 'damageTaken', 100, false);
+        expect(result).to.equal('damageTaken: 80 (80%)\n');
     });
 
-    it('should equal Test: 3 (3%)\n', () => {
+    it('should equal redCoins: 3 (3%)\n', () => {
         const testArray = [{
-            name: 'Test',
+            name: 'redCoins',
             optionalStats: {
                 redCoins: 3
             }
         }];
         const result = tests.arrayToString(testArray, 'redCoins', 100, true);
-        expect(result).to.equal('Test: 3 (3%)\n');
+        expect(result).to.equal('redCoins: 3 (3%)\n');
     });
 });
