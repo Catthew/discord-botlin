25a42b0b66388569a87787370cc74c9fcf574a80
Catthew <matthew.wolfman@outlook.com>
2022-05-08 23:57:31 -0400

    Added more responses for schedule and errors

--- constants/responses.js
+++ constants/responses.js
@@ -1,12 +1,14 @@
 module.exports = {
     access_denied: 'You do not have permission to make that command.',
     bot_at: 'Type gobo! to use me',
+    invalidCommandValue: ['Invalid Command Value', 'Please enter a valid command value.'],
     no_command: 'Please enter a command.',
     no_date_time: 'Please enter a date and/or time.',
     no_info: 'I don\'t know that!',
     schedule_canceled: 'You are safe for another week...',
-    schedule_not_updated: 'Error: The schedule hasn\'t been updateded.',
+    schedule_error: ['Schedule is null', 'Schedule not modified'],
+    schedule_not_updated: 'Error: The schedule hasn\'t been updated.',
     schedule_updated: 'The schedule has been updated.',
-    stats_not_updated: 'Error: The stats hasn\'t been updateded.',
+    stats_not_updated: 'Error: The stats hasn\'t been updated.',
     unknown_command: 'I do not know this command.'
 };
