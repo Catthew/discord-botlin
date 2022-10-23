ab3130f2f73fa4e8e0f4b5177576009653bc50c2
Catthew <mwolfman18@gmail.com>
2022-10-22 23:46:12 -0400

    Use handlers to handle commands and events

---
+++ handlers/handleCommands.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+    client.handleCommands = async () => {
+        const commandFiles = fs
+            .readdirSync('./commands')
+            .filter((file) => file.endsWith('.js'));
+        const { commands, commandArray } = client
+        for (const file of commandFiles) {
+            const command = require(`../../commands/${file}`);
+            commands.set(command.data.name, command);
+            commandArray.push(command.data.toJSON());
+        }
+    };
+};
