d061a878204e69d1dabb3db807eecced2b781e38
Catthew <mwolfman18@gmail.com>
2022-10-24 21:45:12 -0400

    removed unused param

--- commands/info.js
+++ commands/info.js
@@ -14,7 +14,7 @@ module.exports = {
     async execute(args, client, message) {
         const response = capitalize(args.join(' '));
         let info;
-        try { info = await searchForInfo(client, message, response); }
+        try { info = await searchForInfo(client, response); }
         catch (error) {
             common.logAndSendError(error, filename, message, responses['info_error'][1]);
             return;
@@ -50,7 +50,15 @@ module.exports = {
                 embeds: [infoEmbed]
             }).catch(console.error);
         }
-    }
+    },
+    arrayToString,
+    capitalize,
+    getBuilding,
+    getCharacter,
+    getLocation,
+    getNPC,
+    searchForInfo,
+    splitMutlipleSentences
 };
 
 /**
@@ -159,11 +167,10 @@ function getNPC(info) {
 /**
  * Tries to retrieve the search term from the database.
  * @param {Discord.Client} client The client instance of the bot.
- * @param {Discord.Message} message The message object that triggered this method.
  * @param {String} term The term the user is searching for.
  * @returns {?String} null if not found, or an array of the found object and a string denoting the kind of object.
  */
-async function searchForInfo(client, message, term) {
+async function searchForInfo(client, term) {
     try {
         const building = await client.getBuilding(term);
         if (building != null) return [building, 'Building'];
@@ -187,14 +194,3 @@ async function searchForInfo(client, message, term) {
 function splitMutlipleSentences(info) {
     return info.split('.').join('.\n');
 }
-
-exports.tests = {
-    arrayToString,
-    capitalize,
-    getBuilding,
-    getCharacter,
-    getLocation,
-    getNPC,
-    searchForInfo,
-    splitMutlipleSentences
-};
