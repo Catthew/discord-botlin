f4f3e522087855722a5a31a25df902ca0c3701c1
Catthew <mwolfman18@gmail.com>
2022-10-24 20:23:55 -0400

    updated stats to new format

--- commands/stats.js
+++ commands/stats.js
@@ -1,3 +1,6 @@
+const {
+    SlashCommandBuilder
+} = require('discord.js');
 const common = require('../utils/common_functions');
 const excelStats = require('../cron_jobs/scripts/excel_stats');
 const info = require('./stats/stats_info');
@@ -5,21 +8,20 @@ const responses = require('../utils/constants/responses');
 
 const filename = __filename.slice(__dirname.length + 1);
 
-/**
- * Checks which stats command was called.
- * @param {Array.<String>} args The message the user sent split into any array of words.
- * @param {Discord.Client} client The client instance of the bot.
- * @param {Discord.Message} message The message object that triggered this method.
- */
-exports.run = async (args, client, message) => {
-    switch (args[0]) {
-        case 'sync':
-            if (common.isAdmin(message)) excelStats.syncStats(process.env.SPREADSHEET, client, true);
-            break;
-        case undefined:
-            info.getStatsInfo(client, message);
-            break;
-        default:
-            common.logAndSendError(responses['unknown_command'], filename, message, responses['unknown_command']);
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('Checks which stats command was called.'),
+    async execute(args, client, message) {
+        switch (args[0]) {
+            case 'sync':
+                if (common.isAdmin(message)) excelStats.syncStats(process.env.SPREADSHEET, client, true);
+                break;
+            case undefined:
+                info.getStatsInfo(client, message);
+                break;
+            default:
+                common.logAndSendError(responses['unknown_command'], filename, message, responses['unknown_command']);
+        }
     }
 };
