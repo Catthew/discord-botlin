a1a97d261b666c7a8eb0989984f6ec735b83c8d2
Catthew <mwolfman18@gmail.com>
2022-11-08 22:45:02 -0500

    Updated responses location

--- utils/common_functions.js
+++ utils/common_functions.js
@@ -1,4 +1,4 @@
-const responses = require('./constants/responses');
+const { Responses } = require('../utils');
 
 /**
  * Checks to see if an admin made the command
@@ -8,7 +8,7 @@ const responses = require('./constants/responses');
 function isAdmin(message) {
     const author = message.author.id;
     if (author !== process.env.BOTOWNER && author !== process.env.DM) {
-        message.channel.send(responses['access_denied']).catch(console.error);
+        message.channel.send(Responses['access_denied']).catch(console.error);
         return false;
     } else return true;
 }
@@ -20,7 +20,7 @@ function isAdmin(message) {
  * @param {Discord.Message} message The message object that triggered this method.
  * @param {String} response What is sent to Discord.
  */
-function logAndSendError(error, FILENAME, message, response) {
+function logAndSendError(error, filename, message, response) {
     console.log(filename + ': ' + error);
     if (message !== null) message.channel.send(response).catch(console.error);
 }
--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,4 @@
-
-
-
-
 module.exports = {
     Common: require('./common_functions'),
-    Modes: require('../commands/schedules/utils/modes'),
-    OptionalStats: require('./optional_stats'),
     Responses: require('./responses')
 };
