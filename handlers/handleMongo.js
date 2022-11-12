4049719a4fc01fe5a12b1368241b9dabf0ef8e8e
Catthew <mwolfman18@gmail.com>
2022-11-12 00:37:43 -0500

    command is not used

--- handlers/handleMongo.js
+++ handlers/handleMongo.js
@@ -5,7 +5,6 @@ module.exports = client => {
         const commandFiles = fs
             .readdirSync('./mongo/modules')
             .filter((file) => file.endsWith('.js'));
-        const { commands } = client
         for (const file of commandFiles) {
             require(`../mongo/modules/${file}`)(client);
         }
